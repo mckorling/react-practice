06b09ec83f0ddbbff361890c76e597bc38a6088b
Megan Korling <korlingm@gmail.com>
2023-04-30 15:05:46 -0700

    Add image carousel

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import { useState } from 'react';
 import './App.css';
-import Clock from './Clock';
-import CountrySelect from './CountrySelect';
-import CreateAppForm from './CreateForm';
-import PokeDex from './PokeDex';
-import ToDo from './ToDo';
-import TrafficLight from './TrafficLight';
-import SearchBar from './searchbar/SearchBar';
-import SearchResultsList from './searchbar/SearchResultsList';
+import Carousel from './imagecarousel/Carousel';
+import slides from '../src/imagecarousel/data/carouselData.json'
+// import Clock from './Clock';
+// import CountrySelect from './CountrySelect';
+// import CreateAppForm from './CreateForm';
+// import PokeDex from './PokeDex';
+// import ToDo from './ToDo';
+// import TrafficLight from './TrafficLight';
+// import SearchBar from './searchbar/SearchBar';
+// import SearchResultsList from './searchbar/SearchResultsList';
 
 function App() {
   const [results, setResults] = useState([]);
 
   return (
     <div className="App">
-      <SearchBar setResults={setResults} />
-      <SearchResultsList results={results} />
+      <Carousel data={slides.slides} />
+      {/* <SearchBar setResults={setResults} />
+      <SearchResultsList results={results} /> */}
       {/* <CreateAppForm></CreateAppForm>
       <CountrySelect></CountrySelect> */}
       {/* <PokeDex></PokeDex> */}
